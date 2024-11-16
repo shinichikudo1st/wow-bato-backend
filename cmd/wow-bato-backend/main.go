9a7dc9da3fbe9216f24cb471aff81328bcdaf78e
unknown <iam180622@gmail.com>
2024-11-16 19:33:33 +0800

    fixed CORS issue allowed origin

--- cmd/wow-bato-backend/main.go
+++ cmd/wow-bato-backend/main.go
@@ -24,7 +24,7 @@ func main() {
 	router.Use(sessions.Sessions("mysession", store))
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+        AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
