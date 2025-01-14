4048456559463509b39c6d50349f2f8ebc15c7f6
Shinichi Kudo <iam180622@gmail.com>
2025-01-14 21:06:44 +0800

    budgetItemRoute.go documentation

--- internal/routes/budgetItemRoute.go
+++ internal/routes/budgetItemRoute.go
@@ -6,7 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterBudgetItemRoutes(router *gin.RouterGroup){
+// RegisterBudgetItemRoutes registers budget item-related routes on the provided router group.
+//
+// This route contains:
+// - POST /budgetItem/add/:categoryID: Handles adding a new budget item.
+// - GET /budgetItem/all/:projectID: Handles retrieving a list of budget items.
+// - GET /budgetItem/:projectID/:budgetItemID: Handles retrieving a single budget item.
+// - PUT /budgetItem/:budgetItemID: Handles updating a budget item.
+func RegisterBudgetItemRoutes(router *gin.RouterGroup) {
 	budgetItem := router.Group("/budgetItem")
 	{
 		budgetItem.POST("/add/:categoryID", handlers.AddNewBudgetItem)
