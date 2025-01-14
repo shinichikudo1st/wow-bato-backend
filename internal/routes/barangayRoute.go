39c9f2336d498402b94489c3d836cf67f6989575
Shinichi Kudo <iam180622@gmail.com>
2025-01-14 21:04:44 +0800

    barangayRoute.go documentation

--- internal/routes/barangayRoute.go
+++ internal/routes/barangayRoute.go
@@ -6,7 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterBarangayRoute(router *gin.RouterGroup){
+// RegisterBarangayRoute registers barangay-related routes on the provided router group.
+//
+// This route contains:
+// - POST /barangay/add: Handles adding a new barangay.
+// - DELETE /barangay/delete/:barangay_ID: Handles deleting a barangay.
+// - PUT /barangay/update/:barangay_ID: Handles updating a barangay.
+// - GET /barangay/all: Handles retrieving a list of barangays.
+// - GET /barangay/single/:barangay_ID: Handles retrieving a single barangay.
+// - GET /barangay/options: Handles retrieving barangay used for dropdown selection
+func RegisterBarangayRoute(router *gin.RouterGroup) {
 	barangay := router.Group("/barangay")
 	{
 		barangay.POST("/add", handlers.AddBarangay)
@@ -14,6 +23,6 @@ func RegisterBarangayRoute(router *gin.RouterGroup){
 		barangay.PUT("/update/:barangay_ID", handlers.UpdateBarangay)
 		barangay.GET("/all", handlers.GetAllBarangay)
 		barangay.GET("/single/:barangay_ID", handlers.GetSingleBarangay)
-        barangay.GET("/options", handlers.GetBarangayOptions)
+		barangay.GET("/options", handlers.GetBarangayOptions)
 	}
 }
