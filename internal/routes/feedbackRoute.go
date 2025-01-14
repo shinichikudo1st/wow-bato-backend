4f584c472d3679100e6b253c634f3abe7ef46c9b
Shinichi Kudo <iam180622@gmail.com>
2025-01-14 21:03:35 +0800

    feedbackRoute.go documentation

--- internal/routes/feedbackRoute.go
+++ internal/routes/feedbackRoute.go
@@ -6,11 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// RegisterFeedbackRoutes registers feedback-related routes on the provided router group.
+//
+// This route contains:
+// - POST /feedback/create/:projectID: Handles creating a new feedback/comment.
+// - GET /feedback/all/:projectID: Handles retrieving a list of feedbacks/comment.
+// - PUT /feedback/update/:feedbackID: Handles updating a feedback/comment.
+// - DELETE /feedback/delete/:feedbackID: Handles deleting a feedback/comment.
 func RegisterFeedbackRoutes(router *gin.RouterGroup) {
 	feedback := router.Group("/feedback")
 	{
-        feedback.POST("/create/:projectID", handlers.CreateFeedBack)
-        feedback.GET("/all/:projectID", handlers.GetAllFeedbacks)
+		feedback.POST("/create/:projectID", handlers.CreateFeedBack)
+		feedback.GET("/all/:projectID", handlers.GetAllFeedbacks)
 		feedback.PUT("/update/:feedbackID", handlers.EditFeedback)
 		feedback.DELETE("/delete/:feedbackID", handlers.DeleteFeedback)
 	}
