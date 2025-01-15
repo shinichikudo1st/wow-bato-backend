0699dc608720645fcc31e119c60c9b2ea3887ef4
Kōkōsei tantei <126427892+shinichikudo1st@users.noreply.github.com>
2025-01-15 20:27:59 +0800

    Update barangayModel.go

--- internal/models/barangayModel.go
+++ internal/models/barangayModel.go
@@ -6,6 +6,8 @@ type AddBarangay struct {
 	Region string `json:"region"`
 }
 
+// CHANGE LATER TO NOT PUT BRGY. ID IN THE JSON BODY
+// BUT AS A PARAMETER IN THE URL
 type DeleteBarangay struct {
 	Barangay_ID uint `json:"barangay_ID"`
 }
@@ -16,6 +18,7 @@ type UpdateBarangay struct {
 	Region string `json:"region"`
 }
 
+// used for displaying brgy. information
 type AllBarangayResponse struct {
 	ID     uint   `json:"id"`
 	Name   string `json:"name"`
@@ -23,6 +26,7 @@ type AllBarangayResponse struct {
 	Region string `json:"region"`
 }
 
+// returned to the client for selection inputs
 type OptionBarangay struct {
     ID      uint `json:"id"`
     Name    string `json:"name"`
