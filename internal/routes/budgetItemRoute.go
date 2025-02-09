c3555af30e257781830b4dc002ff81ad1c9c86a2
Kōkōsei tantei <126427892+shinichikudo1st@users.noreply.github.com>
2025-02-09 21:33:03 +0800

    Update budgetItemRoute.go

--- internal/routes/budgetItemRoute.go
+++ internal/routes/budgetItemRoute.go
@@ -13,6 +13,7 @@ import (
 // - GET /budgetItem/all/:projectID: Handles retrieving a list of budget items.
 // - GET /budgetItem/:projectID/:budgetItemID: Handles retrieving a single budget item.
 // - PUT /budgetItem/:budgetItemID: Handles updating a budget item.
+// - DELETE /delete/:budgetItemID: Handles the deletion of budget item
 func RegisterBudgetItemRoutes(router *gin.RouterGroup) {
 	budgetItem := router.Group("/budgetItem")
 	{
@@ -20,6 +21,6 @@ func RegisterBudgetItemRoutes(router *gin.RouterGroup) {
 		budgetItem.GET("/all/:projectID", handlers.GetAllBudgetItem)
 		budgetItem.GET("/:projectID/:budgetItemID", handlers.GetSingleBudgetItem)
 		budgetItem.PUT("/update-status/:budgetItemID", handlers.UpdateStatusBudgetItem)
-		budgetItem.DELETE("/delete/:budgetItemID")
+		budgetItem.DELETE("/delete/:budgetItemID", handlers.DeleteBudgetItem)
 	}
 }
